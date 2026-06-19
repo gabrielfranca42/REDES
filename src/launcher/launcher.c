27eff41599f502abf87f1239e9ec0459073161ac
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-06-19 16:39:04 -0300

    hotfix: erro de syntaxe e adição OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS)

--- src/launcher/launcher.c
+++ src/launcher/launcher.c
@@ -2,7 +2,7 @@
  #include <windows.h>
  #include <tchar.h>
 
- #define SERVICO_NOME       _T("MeuServicoSystem");
+ #define SERVICO_NOME       _T("MeuServicoSystem")
  #define SERVICO_EXIBICAO    _T("Meu Servico de Otimizacao Core")
  #define SERVICE_EXE_NOME    _T("service.exe")
  
@@ -25,18 +25,18 @@ BOOL ObterCaminhoService(TCHAR* buffer, DWORD tamanhoBuffer)
     *(ultimaBarra + 1) = TEXT('\0');
     
     
-    _sntprinf(bufferCaminho, tamanhoBuffer, _T  ("%s%s"), caminhoLauncher, SERVICE_EXE_NOME);
+    _sntprintf(buffer, tamanhoBuffer, _T("%s%s"), caminhoLauncher, SERVICE_EXE_NOME);
     
     return TRUE;
 
 }
 
-int main () 
-SC_HANDLE hSCManager = NUll;
-SC_HANDLE hServico = NULL;
-TCHAR szBinPath[MAX_PATH];
+int main () {
+    SC_HANDLE hSCManager = NULL;
+    SC_HANDLE hService = NULL;
+    TCHAR szBinPath[MAX_PATH];
 
-_tprinf(_T("[+]Iniciando o laucer/instalador ...\n"));
+    _tprintf(_T("[+] Iniciando o launcher/instalador...\n"));
 
 if (!ObterCaminhoService(szBinPath,MAX_PATH))
 {
@@ -44,33 +44,39 @@ if (!ObterCaminhoService(szBinPath,MAX_PATH))
     return 1;
 }
 
-hService = CreateService(
-    hSCManager,
-    SERVICO_NOME,
-    SERVICO_EXIBICAO,
-    SERVICE_ALL_ACCESS,
-    SERVICE_WIN32_OWN_PROCESS,
-    SERVICE_DEMAND_START,
-    SERVICE_ERROR_NORMAL,
-    szBinPath,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-);
-
-if (hService == NULL){
-    DWORD erro = GetLastError(0);
-
-    if (erro == ERROR_SERVICE_EXISTS){
-        _tprintf(_T("[*] O servico '&s' ja esta registraddo. Abrindo configuração...\n"), SERVICO_NOME)
-        hService = OpenService(hSCManager, SERVICO_NOME,SERVICE_ALL_ACCESS);
-    } else {
-        printf("[-]Erro critico ao criar servico. Codigo: %lu\n", erro );
-        CloseServiceHandle(hSCManager);
+    hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+    if (hSCManager == NULL) {
+        printf("[-] Erro ao abrir o Service Control Manager. Codigo: %lu\n", GetLastError());
         return 1;
     }
+
+    hService = CreateService(
+        hSCManager,
+        SERVICO_NOME,
+        SERVICO_EXIBICAO,
+        SERVICE_ALL_ACCESS,
+        SERVICE_WIN32_OWN_PROCESS,
+        SERVICE_DEMAND_START,
+        SERVICE_ERROR_NORMAL,
+        szBinPath,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    );
+
+    if (hService == NULL){
+        DWORD erro = GetLastError();
+
+        if (erro == ERROR_SERVICE_EXISTS){
+            _tprintf(_T("[*] O servico '%s' ja esta registrado. Abrindo configuracao...\n"), SERVICO_NOME);
+            hService = OpenService(hSCManager, SERVICO_NOME,SERVICE_ALL_ACCESS);
+        } else {
+            printf("[-]Erro critico ao criar servico. Codigo: %lu\n", erro );
+            CloseServiceHandle(hSCManager);
+            return 1;
+        }
 } else{
    _tprintf(_T("[+] Servico '%s' registrado com sucesso como SYSTEM!\n"), SERVICO_NOME);
 }
@@ -88,8 +94,8 @@ if (!StartService(hService, 0, NULL)){
     _tprintf(_T("[+] Servico iniciado com sucesso.\n"));
 }
 
-if (hServico){
-    CloseServiceHandle(hServico);
+if (hService){
+    CloseServiceHandle(hService);
 }
 if (hSCManager){
     CloseServiceHandle(hSCManager);
